421bee4307ff105838b2788933e0796d7fb21681
Alba Gustems <albagustems@gmail.com>
2020-08-02 11:45:12 +0200

    Delete and patch routes finished. Highlights route  modified

--- routes/places.js
+++ routes/places.js
@@ -7,118 +7,139 @@ const uploader = require('../configs/cloudinary-setup')
 const Place = require('../models/place')
 
 router.get('/', (req, res, next) => {
-  Place.find()
-    .then(resp => res.status(200).json(resp))
-    .catch(err => next(err))
+    Place
+        .find()
+        .then(resp => res.status(200).json(resp))
+        .catch(err => next(err))
 })
 
 router.get('/highlights', (req, res, next) => {
-
-  const quantity = req.query.quantity
-
-  console.log(quantity)
-
-  Place.find().limit(3)
-    .then(resp => res.status(200).json(resp))
-    .catch(err => next(err))
-
-  
-
+    Place
+        .find({highlight:true})
+        .limit(3)
+        .then(resp => res.status(200).json(resp))
+        .catch(err => next(err))
 })
 
 router.get("/search", (req, res, next) => {
 
-  const miles = req.query.miles
-  const lat = req.query.lat
-  const lng = req.query.lng
-  console.log("Recibiendo query params miles, lat, lng: ", miles, lat, lng)
+    const miles = req.query.miles
+    const lat = req.query.lat
+    const lng = req.query.lng
+    console.log("Recibiendo query params miles, lat, lng: ", miles, lat, lng)
 
-  const milesToRadian = function (miles) {
-    var earthRadiusInMiles = 3959;
-    return miles / earthRadiusInMiles;
-  }
-
-  const query = {
-    "loc": {
-      $geoWithin: {
-        $centerSphere: [[lat, lng], milesToRadian(miles)]
-      }
+    const milesToRadian = function (miles) {
+        var earthRadiusInMiles = 3959;
+        return miles / earthRadiusInMiles;
     }
-  }
 
-  Place.find(query)
-    .then(resp => res.status(200).json(resp))
-    .catch(err => next(err))
+    const query = {
+        "loc": {
+            $geoWithin: {
+                $centerSphere: [
+                    [
+                        lat, lng
+                    ],
+                    milesToRadian(miles)
+                ]
+            }
+        }
+    }
+
+    Place
+        .find(query)
+        .then(resp => res.status(200).json(resp))
+        .catch(err => next(err))
 
 })
 
 router.get('/:id', (req, res, next) => {
-  Place.findById(req.params.id)
-    .then(place => {
-      res.json(place)
-    })
-    .catch(err => {
-      res.json(err)
-    })
+    Place
+        .findById(req.params.id)
+        .then(place => {
+            res.json(place)
+        })
+        .catch(err => {
+            res.json(err)
+        })
 })
 
 router.post('/', uploader.single("imageUrl"), (req, res, next) => {
-  if (!req.file) {
-    next(new Error('No file uploaded!'));
-    return;
-  }
-
-  const coordinates= []
-  coordinates.push(req.body.lat)
-  coordinates.push(req.body.lng)
-
-  Place.create( {
-    name: req.body.name,
-    description: req.body.description,
-    imageUrl: req.file.path,
-    loc:{
-      coordinates:coordinates
+    if (!req.file) {
+        next(new Error('No file uploaded!'));
+        return;
     }
-  })
-    .then(place => {
-      console.log('Created new thing: ', place);
-      res.status(200).json(place)
-      return res.redirect('/list');
+
+    const coordinates = []
+    coordinates.push(req.body.lat)
+    coordinates.push(req.body.lng)
+
+    Place
+        .create({
+        name: req.body.name,
+        description: req.body.description,
+        imageUrl: req.file.path,
+        loc: {
+            coordinates: coordinates
+        }
     })
-    .catch(err => next(err))
+        .then(place => {
+            console.log('Created new thing: ', place);
+            res
+                .status(200)
+                .json(place)
+            return res.redirect('/list');
+        })
+        .catch(err => next(err))
 })
 
 router.delete('/:id', (req, res, next) => {
-  // to do
-  //req.params.id
-  res.status(200).json({ message: "DELETE aguardando implementacion. " })
-
+    res
+        .status(200)
+        .json({message: "DELETE aguardando implementacion. "})
+    Place
+        .deleteOne({_id: req.params.id})
+        .then(() => {
+            res.json({message: `Place id ${req.params.id} deleted successfully`})
+        })
+        .catch(error => res.json(error))
 })
 
 router.patch('/:id', (req, res, next) => {
-  // to do
-  //req.params.id
-  res.status(200).json({ message: "PATCH aguardando implementacion. " })
+    res
+        .status(200)
+        .json({message: "PATCH aguardando implementacion. "})
 
+    Place
+        .findByIdAndUpdate(req.params.id, {highlight: req.body.highlight})
+        .then(() => {
+            res.json({message: `Place id ${req.params.id} updated successfully.`})
+        })
+        .catch(error => {
+            res.json(error)
+        })
 })
 
-router.put('/:id',  uploader.single("imageUrl"), (req, res, next) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    res.status(400).json({ message: 'Specified id is not valid' })
-    return
-  }
-  
-  Place.findByIdAndUpdate(req.params.id, {
-    name: req.body.name,
-    description: req.body.description,
-    imageUrl: req.file.path
-  })
-    .then(() => {
-      res.json({ message: `Place id ${req.params.id} updated successfully.` })
+router.put('/:id', uploader.single("imageUrl"), (req, res, next) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        res
+            .status(400)
+            .json({message: 'Specified id is not valid'})
+        return
+    }
+
+    Place
+        .findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        description: req.body.description,
+        imageUrl: req.file.path
     })
-    .catch(error => {
-      res.json(error)
-    })
+        .then(() => {
+            res.json({message: `Place id ${req.params.id} updated successfully.`})
+        })
+        .catch(error => {
+            res.json(error)
+        })
 })
 
 module.exports = router
